36b1aa052ef550e0c5803054b06876e093f5aa79
Roy <J.jyun0525@gmail.com>
2022-02-09 11:16:41 +0900

    학생 목록을 생성 실제 목록 추가

--- app/src/main/java/com/example/listviewpractice_yun/MainActivity.kt
+++ app/src/main/java/com/example/listviewpractice_yun/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.listviewpractice_yun
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listviewpractice_yun.datas.Student
 
 class MainActivity : AppCompatActivity() {
+
+    //멤버변수
+
+    val mStudentList =ArrayList<Student>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //목록에 뭔가 추가할꺼다.
+        mStudentList.add( Student("윤지",1993))
+        mStudentList.add( Student("김현상",1972))
+        mStudentList.add( Student("고소영",1986))
+        mStudentList.add( Student("전지현",1975))
+        mStudentList.add( Student("쿠키몬",2010))
+        mStudentList.add( Student("쿠마몬",1998))
+
+
     }
 }
