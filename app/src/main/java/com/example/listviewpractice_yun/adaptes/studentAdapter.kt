d038e5eec3e2392cd1aeba1507f1499c1a65c34d
Roy <J.jyun0525@gmail.com>
2022-02-09 11:08:34 +0900

    Adapter 뼈대 작업완료

--- app/src/main/java/com/example/listviewpractice_yun/adaptes/studentAdapter.kt
+++ app/src/main/java/com/example/listviewpractice_yun/adaptes/studentAdapter.kt
@@ -1,11 +1,31 @@
 package com.example.listviewpractice_yun.adaptes
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.listviewpractice_yun.R
 import com.example.listviewpractice_yun.datas.Student
 
 class studentAdapter(
     val mContext: Context,
     val resId:Int,
     val mList : ArrayList<Student>) : ArrayAdapter<Student>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.student_list_item,null)
+        }
+        val row = tempRow!!
+        //temprow가 절대 null이 아니다!!
+
+        return row
+        //완성된 결과=row
+
+    }
 }
